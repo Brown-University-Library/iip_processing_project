b18bc11a257330fa7239118bffc218eef54e8629
Birkin James Diana <birkin.diana@gmail.com>
2022-03-25 10:54:01 -0400

    improves failed-queue output.

--- iip_processing_app/lib/queue_utils/clean_failed_queue.py
+++ iip_processing_app/lib/queue_utils/clean_failed_queue.py
@@ -18,14 +18,14 @@ try:
 
     failed_queue = rq.queue.get_failed_queue( connection=redis.Redis('localhost') )
 
-    output_dct: dict = { 'jobs': [], 'number_of_failed_jobs_deleted': 0 }
-    failed_count: int = 0
+    output_dct: dict = {}
+    failed_iip_count: int = 0
     failed_iip_jobs: list = []
     for job in failed_queue.jobs:
         if not job.origin == QUEUE_NAME:
             print( f'job.id, ``{job._id}`` not a failed iip job; skipping' )
             continue
-        failed_count += 1
+        failed_iip_count += 1
         job_d = {
             '_args': job._args,
             '_kwargs': job._kwargs,
@@ -43,8 +43,9 @@ try:
         }
         failed_iip_jobs.append( job_d )
         job.delete()
-    output_dct['jobs'] = failed_iip_jobs
-    output_dct['number_of_failed_iip_jobs_deleted'] = failed_count
+    output_dct['failed_iip_jobs'] = failed_iip_jobs
+    output_dct['number_of_failed_iip_jobs_deleted'] = failed_iip_count
+    output_dct['number_of_non_iip_failed_jobs'] = len( failed_queue.jobs ) - failed_iip_count
 
     # q2 = rq.Queue( QUEUE_NAME, connection=redis.Redis() )
     # d['current_failed_target_count'] = len(q2.jobs)
